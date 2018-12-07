d3d606e4f3d6f5ea0e3bf7bea2baa8141ffa6044
Henry Asante <henryasante10@gmail.com>
2018-12-07 07:25:35 +0000

    feat: create SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './SearchBar';
 
 
 
 class App extends React.Component {
 	render (){
-		return <div>Hello Youtube</div>;
+		return (
+			<div>
+				<SearchBar />
+			</div>)
 	}
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component {
+	render(){
+		return (
+			<div>
+				<input />
+			</div>
+			)
+	}
+}
+
+
+
+export default SearchBar;
