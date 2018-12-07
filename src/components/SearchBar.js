d5df14084a21b46ef35f09958e71e5d61417f584
Henry Asante <henryasante10@gmail.com>
2018-12-07 07:47:06 +0000

    feat: State object initialized for search term
    the value prop of input is set to reference the state value(term).
    onChange prop is linked to onInputChange callback to update the state value as the user types.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+	state = { term: ''};
+
+	onInputChange = (event) => {
+		this.setState({ term: event.target.value })
+	}
+
 	render(){
 		return (
 			<div className="search-bar ui segment">
@@ -10,6 +16,8 @@ class SearchBar extends React.Component {
 						<label>Video Search</label> <br />
 						<input 
 						type="text"
+						value={this.state.term}
+						onChange={this.onInputChange}
 						/>
 					</div>
 				</form>
